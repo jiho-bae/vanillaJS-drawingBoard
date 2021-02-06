9a85fdbc753c53d815bfb734d9f08a195deccf89
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-02-07 00:28:57 +0900

    add filling function.

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const ctx = canvas.getContext("2d");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-canvas.width = 600;
-canvas.height = 600;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 600;
 
-ctx.strokeStlye = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStlye = INITIAL_COLOR ;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+
 
 let painting = false;
 let filling = false;
@@ -53,11 +58,18 @@ const handleModeClick = () => {
     }
 }
 
+const handleCanvasClick = () => {
+    if(filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 } 
 
 if(colors){
